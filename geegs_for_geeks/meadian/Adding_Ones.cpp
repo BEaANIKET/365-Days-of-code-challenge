21dbf776c296f4ecf5117e581a5d6d6c2423a6c1
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-06-11 12:07:47 +0530

    days 82

---
+++ geegs_for_geeks/meadian/Adding_Ones.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    void update(int a[], int n, int updates[], int k)
+    {
+        for(int j = 0; j < k; j++)
+        {
+            a[updates[j]-1] += 1;
+        }
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cnt += a[i];
+            a[i] = cnt;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n]={0}, updates[k]={0};
+		for(int i = 0; i < k; i++)
+		cin>>updates[i];
+		Solution ob;
+		ob.update(a, n, updates, k);
+		
+		for(int i = 0; i < n; i++)
+		cout<<a[i]<<" ";
+		
+		cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
