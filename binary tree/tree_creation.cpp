01768a01ef4b8a853b4f8be0f1684ac7032af41e
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-07-26 23:10:45 +0530

    days 126

--- binary tree/tree_creation.cpp
+++ binary tree/tree_creation.cpp
@@ -34,7 +34,7 @@ Node* buildtree(Node* root){
     return root;
 }
 
-void traversal(Node* root){
+void levaltraversal(Node* root){
     queue<Node* >q;
     q.push(root);
     q.push(NULL);
@@ -51,7 +51,7 @@ void traversal(Node* root){
             }
         }
         else{
-            cout<<temp->data;
+            cout<<temp->data<<" ";
             if(temp->left){
                 q.push(temp->left);
             }
@@ -63,12 +63,91 @@ void traversal(Node* root){
     }
 }
 
+void inorder(Node* root){
+    if(root==NULL){
+        return ;
+    }
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+
+    if(root==NULL){
+        return ;
+    }
+
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+
+}
+
+void postorder(Node* root){
+    if(root==NULL){
+        return ;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+// 1 3 5 7 11 17 -1 -1 -1 -1 -1 -1 -1 
+void buildfromleval(Node* &root){
+    queue<Node*>q;
+    cout<<"Enter data for root :";
+    int Data;
+    cin>>Data;
+    root=new Node(Data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        cout<<"Enter left data of "<<temp->data<<":";
+        int leftdata;
+        cin>>leftdata;
+
+        if(leftdata!=-1){
+            temp->left=new Node(leftdata);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter right data of "<<temp->data<<":";
+        int rightdata;
+        cin>>rightdata;
+
+        if(rightdata!=-1){
+            temp->right=new Node(rightdata);
+            q.push(temp->right);
+        }
+    }
+}
+
 int main(){
     
      Node* root=NULL;
     //  1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-     root=buildtree(root);
-    traversal(root);
+    //  root=buildtree(root);
+        buildfromleval(root);
+        cout<<endl;
+        levaltraversal(root);
+
+    // traversal(root);
+
+    // cout<<"Inorder traversal is ";
+    // inorder(root);
+    // cout<<endl;
      
+    //  cout<<"Preorder traversal is ";
+    //  preorder(root);
+    //  cout<<endl;
+
+    //  cout<<"Postorder traversal is ";
+    //  postorder(root);
+
     return 0;
 } 
