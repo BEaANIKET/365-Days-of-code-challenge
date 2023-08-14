1777bec497e7d68a9720f1f4454005662d2b63ec
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-08-14 23:17:32 +0530

    days 145

--- 1.leedcode/meadian/215. Kth Largest Element in an Array.cpp
+++ 1.leedcode/meadian/215. Kth Largest Element in an Array.cpp
@@ -13,3 +13,15 @@ public:
         return solve(nums,k);
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>q(nums.begin(),nums.end());
+        while(k>1){
+            q.pop();
+            k--;
+        }
+        return q.top();
+    }
+};
