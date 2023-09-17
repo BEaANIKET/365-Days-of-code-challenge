df95c0a2569e61091f33824d6cbbfe691440b27a
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-09-17 15:10:28 +0530

    17/10/2023

---
+++ arrey/135. Candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>left(n,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+               left[i]=left[i-1]+1;
+            }
+        }
+
+        int ans=0;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                if(left[i]<=left[i+1])
+                    left[i]=left[i+1]+1;
+            }
+
+            ans+=left[i];
+        }
+        ans+=left[n-1];
+
+        return ans;
+    }
+};
+
--- binary tree/(A). tree_creation.cpp
+++ binary tree/(A). tree_creation.cpp
@@ -131,8 +131,8 @@ int main(){
     
      Node* root=NULL;
     //  1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
-    //  root=buildtree(root);
-        buildfromleval(root);
+     root=buildtree(root);
+        // buildfromleval(root);
         cout<<endl;
         levaltraversal(root);
 
